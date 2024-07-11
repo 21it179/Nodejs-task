c00017002683c252fc9d9e82d4c3594af090d8ff
21it179 <126048582+21it179@users.noreply.github.com>
2024-07-11 12:34:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+test('1 + 1 = 2', () => {
+  expect(1 + 1).toBe(2);
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+test('1 + 2 = 3', () => {
+  expect(1 + 2).toBe(3);
 });
